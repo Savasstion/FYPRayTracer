f2c4159e90f3b2e6736691bd385944bc7c79622d
Savasstion <savasstion1108@gmail.com>
2025-08-30 15:21:39 +0800

    Update LightTree.h
    use existing AABB struct

--- FYPRayTracer/src/DataStructures/LightTree.h
+++ FYPRayTracer/src/DataStructures/LightTree.h
@@ -1,7 +1,6 @@
 #ifndef LIGHT_TREE_H
 #define LIGHT_TREE_H
-#include <cstdint>
-#include <glm/vec3.hpp>
+#include "../Classes/BaseClasses/AABB.cuh"
 
 class LightTree
 {
@@ -15,18 +14,12 @@ public:
             float theta_o = 0.0f;
             float theta_e = 0.0f;
         };
-
-        struct WorldSpaceBounds
-        {
-            glm::vec3 upperBounds{0.0f};
-            glm::vec3 lowerBounds{0.0f};
-        };
         
         float energy = 0.0f;
         uint32_t numEmitters = 0;
         uint32_t offset = 0;    // >= 0 left child , otherwise emmiter offset
         OrientationBounds bounds_o;
-        WorldSpaceBounds bounds_w;
+        AABB bounds_w;
     };
     
 };
